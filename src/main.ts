03c1c0d6c693badd8f6192bb478de11241333713
Tautvydas Dulskis <tautvydas.dulskis@venipak.com>
2024-10-16 18:52:38 +0300

    Load data from JSON

--- src/main.ts
+++ src/main.ts
@@ -14,20 +14,33 @@ let htmlWriter = (item: IModel): void => {
     listElement.appendChild(listItem);
 }
 const taskPrinter: Printer = new Printer(taskManager, htmlWriter);
+const kamPriklauso = new User('Tautvydas');
 
 // ---------- Funkcijos skirtos Event Listeneriams -------------------
 function createTask(): void {
   const taskElement = document.getElementById("newTaskInput") as HTMLInputElement;
   const taskTitle: string = taskElement.value;
-  const kamPriklauso = new User('Tautvydas');
   const taskItem = new Task (taskTitle, kamPriklauso.getId());
   taskManager.add(taskItem);
   taskPrinter.printAll()
 }
+
+async function PrintAllTasks(): Promise<void> {
+    const tasks: Response = await fetch('db/tasks.json');
+    const tasksJson = await tasks.json();
+
+    tasksJson.forEach((taskData: any): void => {
+        const taskItem = new Task (taskData.title, kamPriklauso.getId());
+        taskManager.add(taskItem);
+    })
+    taskPrinter.printAll(); // Sita vieta pas visus skirsis nuo mano eilutes. Cia tiesiog kvieciam pritinimo funkcija.
+}
+
 // ---------- EVENT Listeneriai --------------------------------------
 const createButton = document.getElementById("createButton") as HTMLButtonElement;
 createButton.addEventListener("click", createTask);
 
+document.addEventListener('DOMContentLoaded', PrintAllTasks);
 
 
 
